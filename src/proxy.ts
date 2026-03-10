b975c23f2324c2c343594cedb4853c377f5203d6
Prithvi Raj <138392587+prithvikings@users.noreply.github.com>
2026-03-10 21:54:23 +0530

    fix the proxy.ts

--- src/proxy.ts
+++ src/proxy.ts
@@ -2,6 +2,7 @@ import { getToken } from "next-auth/jwt";
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
+
 export async function proxy(req: NextRequest) {
   const token = await getToken({ req, secret: process.env.NEXTAUTH_SECRET });
   const { pathname } = req.nextUrl;
